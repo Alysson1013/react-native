f3636488e7d57d01b842325a36b6719f4d08ac7d
Alysson Eduardo <alysson6801@hotmail.com>
2021-08-19 16:20:42 -0300

    chore: user list and form

---
+++ rncrud/src/views/UserList.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { View, Text, FlatList, Alert } from 'react-native'
+import users from '../data/users'
+import { ListItem, Avatar, Button, Icon } from 'react-native-elements'
+
+export default props => {
+
+    function confirmUserDeletion(user){
+        Alert.alert('Excluir Usuario', 'Deseja excluir o usuario?', [
+            {
+                text: 'Sim',
+                onPress(){
+                    
+                }
+            },
+            {
+                text: 'Não'
+            }
+        ])
+    }
+
+    function getUserItem({ item: user }) {
+        return (
+            <ListItem
+                bottomDivider
+                onPress={() => props.navigation.navigate('UserForm')}
+                key={user.id}>
+                <Avatar
+                    title={user.name}
+                    Subtitle={user.email}
+                    source={{ uri: user.avatarUrl }}
+                />
+                <ListItem.Content>
+                    <ListItem.Title>{user.name}</ListItem.Title>
+                    <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+                </ListItem.Content>
+                <Button
+                    onPress={() => {
+                        props.navigation.navigate('UserForm', user);
+                    }}
+                    type="clear"
+                    icon={<Icon name="edit" size={25} color="orange" />}
+                />
+                <Button
+                    onPress={() => { confirmUserDeletion(user) }}
+                    type="clear"
+                    icon={<Icon name="delete" size={25} color="red" />}
+                />
+            </ListItem>
+        )
+    }
+    return (
+        <View>
+            <FlatList
+                keyExtractor={user => user.id.toString()}
+                data={users}
+                renderItem={getUserItem}
+            />
+        </View>
+    )
+}
