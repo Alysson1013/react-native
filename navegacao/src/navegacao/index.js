de32520f8a5a954f3388c3b5494ae935ab56989c
Alysson <alysson6801@hotmail.com>
2021-08-20 10:08:38 -0300

    chore: ignite - jsx sctructe

--- navegacao/src/navegacao/index.js
+++ navegacao/src/navegacao/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Text, SafeAreaView} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native'
+import {NavigationContainer} from '@react-navigation/native';
 import Stack from './Stack';
-import Tab from './Tab'
+import Tab from './Tab';
+import Drawer from './Drawer';
 
 export default props => (
-    <SafeAreaView style={{flex: 1}}>
-        <NavigationContainer>
-            <Tab />
-        </NavigationContainer>
-    </SafeAreaView>
+  <SafeAreaView style={{flex: 1}}>
+    <NavigationContainer>
+      <Tab />
+    </NavigationContainer>
+  </SafeAreaView>
 );
